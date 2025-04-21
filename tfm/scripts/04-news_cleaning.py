91e4494f34642a62eb4e65d6ab7988b4b3cb5527
laura-guerra <laugueriv@gmail.com>
2025-04-21 18:09:40 +0200

    Add market parameter to transform_dataset_by_day method in NLProcessor

--- tfm/scripts/04-news_cleaning.py
+++ tfm/scripts/04-news_cleaning.py
@@ -1,13 +1,23 @@
 """Script to process cleaned news and prepare NLP-ready text."""
 
 import pandas as pd
+from loguru import logger
+
 from tfm.src.config.settings import PATH_DATA_PROCESSED
 from tfm.src.nlp.news_processor import NewsProcessor
 from tfm.src.nlp.text_processor import TextProcessor
+from tfm.src.utils.generate_new_market_date import compute_effective_market_date
 
 # %% Load data
 news_df = pd.read_csv(PATH_DATA_PROCESSED / "articles_cleaned.csv")
 keywords_df = pd.read_csv(PATH_DATA_PROCESSED / "keywords_cleaned.csv")
+
+# %% Ensure datetime with timezone
+news_df["date"] = pd.to_datetime(news_df["date"], utc=True)
+
+# %% Add market-based effective dates
+news_df["market_date_us"] = news_df["date"].apply(lambda x: compute_effective_market_date(x, market="us"))
+news_df["market_date_eu"] = news_df["date"].apply(lambda x: compute_effective_market_date(x, market="eu"))
 
 # %% Unify text
 news_processor = NewsProcessor(
@@ -15,7 +25,6 @@ news_processor = NewsProcessor(
     top_n_keywords=5,
     use_tags=False
 )
-
 news_with_text = news_processor.unify_text(news_df, keywords_df)
 
 # %% Clean text for NLP
@@ -25,4 +34,4 @@ news_final = text_processor.apply(news_with_text, text_column="full_text")
 # %% Save output
 news_final.to_csv(PATH_DATA_PROCESSED / "articles_final_nlp.csv", index=False)
 
-print("Done!")
+logger.info("✅ NLP-ready news data saved.")
